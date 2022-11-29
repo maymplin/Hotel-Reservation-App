13cb7a411fe16961e47128835986601f6d487867
May Lin <92900468+maymplin@users.noreply.github.com>
2022-11-28 19:12:09 -0600

    Rename HoteReservationApplication class

--- src/HotelApplication.java
+++ src/HotelApplication.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class HotelReservationApplication {
+public class HotelApplication {
 
     public static void main(String[] args) {
 
